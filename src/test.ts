b70818680778529604c867d164f120a1dd99b351
Blair Strang <Blair.Strang@auspost.com.au>
2016-03-30 12:05:19 +1100

    Add test.

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,26 @@
-// import * as fs from './index';
+import * as assert from 'assert';
+import * as fs from '@sane/fs';
+import * as fsx from './index';
 
-describe('fs', function() {
-    describe('unlink', function() {
+const TEST_DIR = '/tmp/rimraf';
+
+describe('fsx', function() {
+    describe('rimraf', function() {
+        it('should delete things', async () => {
+            let testFiles = ['/test1', '/test2', '/test3'].map(x => TEST_DIR + x);
+            await fs.mkdirAsync(TEST_DIR);
+            for(let tf of testFiles) {
+                await fs.writeFileAsync(tf, 'Hello World');
+                assert.equal(await fs.readFileAsync(tf), 'Hello World'); /* Paranoia */
+            }
+            await fsx.rimraf(TEST_DIR);
+            try {
+                await fs.statAsync(TEST_DIR);  /* Should throw if rimraf worked */
+            } catch (e) {
+                assert.equal(e.code, 'ENOENT');
+                return;
+            }
+            assert.equal(1, 0); /* NOT REACHED */
+        });
     });
 });
